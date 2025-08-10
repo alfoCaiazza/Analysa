32be0219f46d5985e69fbc0c168d26904881443b
alfoCaiazza <acaiazza2000@gmail.com>
2025-08-10 18:16:11 +0200

    Added 'id' to retrieval query

--- src/nlp/get_graph_textual_information.py
+++ src/nlp/get_graph_textual_information.py
@@ -11,10 +11,10 @@ cursor = connection.cursor()
 
 # Retriving all texts for both posts and comments tables
 cursor.execute('''
-    SELECT p.author, p.text, 'post' AS type
+    SELECT p.author, p.id, p.text, 'post' AS type
     FROM posts as p
         UNION ALL
-    SELECT c.author, c.text, 'comment' AS type
+    SELECT c.author, c.comment_Id, c.text, 'comment' AS type
     FROM comments as c
 ''')
 
@@ -27,5 +27,5 @@ print(f"Operation sample: {res[0]}")
 # Writing results in a csv file
 with open('src/nlp/raw_textual_df.csv', 'w') as csv_file:
     writer = csv.writer(csv_file, delimiter=',')
-    writer.writerow(['author', 'text', 'type'])
+    writer.writerow(['author', 'id', 'text', 'type'])
     writer.writerows(res)
