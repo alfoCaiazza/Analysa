1975685dbf49d27b63a1009ab6f2f29062350bd1
alfoCaiazza <acaiazza2000@gmail.com>
2025-08-10 12:09:14 +0200

    Updated query: now all texts of the same author are reported separatelly and not in the same 'cell'. Also, added an attribute 'type' that indicates iof the text is a comment or a post

--- src/nlp/get_graph_textual_information.py
+++ src/nlp/get_graph_textual_information.py
@@ -11,14 +11,11 @@ cursor = connection.cursor()
 
 # Retriving all texts for both posts and comments tables
 cursor.execute('''
-    SELECT author, GROUP_CONCAT(text, '||') AS all_texts
-    FROM (
-        SELECT p.author, p.text
-        FROM posts as p
-            UNION ALL
-        SELECT c.author, c.text
-        FROM comments as c
-    ) GROUP BY author
+    SELECT p.author, p.text, 'post' AS type
+    FROM posts as p
+        UNION ALL
+    SELECT c.author, c.text, 'comment' AS type
+    FROM comments as c
 ''')
 
 res = cursor.fetchall()
@@ -30,5 +27,5 @@ print(f"Operation sample: {res[0]}")
 # Writing results in a csv file
 with open('src/nlp/raw_textual_df.csv', 'w') as csv_file:
     writer = csv.writer(csv_file, delimiter=',')
-    writer.writerow(['author', 'text'])
+    writer.writerow(['author', 'text', 'type'])
     writer.writerows(res)
