f8b51a253504d2027d50343bb07ef872d1d00ba4
alfoCaiazza <acaiazza2000@gmail.com>
2025-08-23 15:33:57 +0200

    Developed script for emotional analysis based on goemotion model and adjusting topic modeling BERTopic model parameters

--- src/nlp/get_graph_textual_information.py
+++ src/nlp/get_graph_textual_information.py
@@ -13,19 +13,34 @@ cursor = connection.cursor()
 cursor.execute('''
     SELECT p.author, p.id, p.text, 'post' AS type
     FROM posts as p
+    WHERE p.text <> '[removed]'
         UNION ALL
     SELECT c.author, c.comment_Id, c.text, 'comment' AS type
     FROM comments as c
+    WHERE C.text <> '[removed]'
 ''')
 
 res = cursor.fetchall()
 cursor.close()
 
-print(f"Total texts retrieved: {len(res)}")
 print(f"Operation sample: {res[0]}")
+
+# Retrieving valid users
+users = pd.read_csv('src/data/nodes.csv')
+users_id = users['id'].tolist()
+valid_users_set =set(users_id)
+
+count = 0
 
 # Writing results in a csv file
 with open('src/nlp/raw_textual_df.csv', 'w') as csv_file:
     writer = csv.writer(csv_file, delimiter=',')
     writer.writerow(['author', 'id', 'text', 'type'])
-    writer.writerows(res)
+
+    for row in res:
+        if row[0] in valid_users_set:
+            count += 1
+            writer.writerow(row)
+
+print(f"Total texts retrieved: {len(res)}")
+print(f"Total texts with valid user: {count}")
